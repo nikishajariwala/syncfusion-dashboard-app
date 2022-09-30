86eba45705ff9cfd372b2d624e0e96801970dd4e
Dr. Nikisha Jariwala, Ph.D <106479727+nikishajariwala@users.noreply.github.com>
2022-09-30 16:48:08 +0530

    Add files via upload

---
+++ src/pages/Charts/Stacked.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import { Header } from "../../components";
+
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Stacked" title="Stacked Chart" />
+      <ChartComponent
+        id="charts"
+        primaryXAxis={stackedPrimaryXAxis}
+        primaryYAxis={stackedPrimaryYAxis}
+        width={width}
+        height={height}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        legendSettings={{ background: "white" }}
+      >
+        <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+        <SeriesCollectionDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {stackedCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
+
+export default Stacked;
